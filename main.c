bf8ac617af897afdfc459de9838d9ef4e56db992
lt2415 <lt2415@mecws771-001.ic.ac.uk>
2017-11-13 14:08:27 +0000

    Lab 5 - Ex1b - Complete

--- main.c
+++ main.c
@@ -20,29 +20,33 @@ void main(void){
     
     initPWM(); //setup PWM registers
     while(1){
-        angle2PWM(-90); //set servo to -90 deg
-        delay_s(1);//wait for 1s
-        angle2PWM(90); //set servo to 90 deg
-        delay_s(1);//wait for 1s
+        for (int i = -90; i < 90; i++){     // Increment from -90 to 90 degrees
+            angle2PWM(i);
+            __delay_ms(50);                 // Increment 1 degree every 0.05 seconds (~20 degrees per second)
+        }
+        for (int i = -90; i < 90; i++) {    // Reverse direction, as before
+            angle2PWM(-1*i);
+            __delay_ms(50);
+        }
     }
 }
 
 void initPWM() {
-    PTCON0 = 0b00001100; // free running mode, 1:64 prescaler = 32 us
-    PTCON1 = 0b10000000; // enable PWM timer
-    PWMCON0 = 0b00101111; // PWM0/1 enabled, all independent mode
-    PWMCON1 = 0x00; // special features, all 0 (default)
+    PTCON0 = 0b00001100;    // free running mode, 1:64 prescaler = 32 us
+    PTCON1 = 0b10000000;    // enable PWM timer
+    PWMCON0 = 0b00101111;   // PWM0/1 enabled, all independent mode
+    PWMCON1 = 0x00;         // special features, all 0 (default)
     int PTPER_aim = 624;
     PTPERL = PTPER_aim & 0b0000000011111111; // base PWM period low byte
     PTPERH = PTPER_aim >> 8; // base PWM period high byte
 }
 
 void angle2PWM(int angle) {
-    int T_aim = ((200*angle)/225 + 130); //This doesnt work
-    int PDC0_aim = getPT(T_aim, 8, 64);  //
+    int T_aim = ((200*angle)/225 + 130);    // Apply angle range to PWM range
+    int PDC0_aim = getPT(T_aim, 8, 64);     //
     //note: the two LSBs in PDC0L have a different function;
-    PDC0L = (PDC0_aim & 0x3F) << 2; // PDC0L bits 7:2 are on period bits 5:0 - take bottom 5 and shift to fit
-    PDC0H = (PDC0_aim & 0xFC0) >> 6; // PDC0H bits 5:0 are on period bits 11:6
+    PDC0L = (PDC0_aim & 0x3F) << 2;         // PDC0L bits 7:2 are on period bits 5:0 - take bottom 5 and shift to fit
+    PDC0H = (PDC0_aim & 0xFC0) >> 6;        // PDC0H bits 5:0 are on period bits 11:6
 }
 
 // Function to get value of Tpwm calculations, T in 100ms, OSC in MHz, scaler 
