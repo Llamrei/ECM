4cd70a1fb2a83f020269e63b5955e0dab5edd856
al3615 <al3615@mecws771-003.ic.ac.uk>
2017-11-13 15:37:49 +0000

    Lab 5 - Ex3 - Imported code

--- main.c
+++ main.c
@@ -1,48 +1,22 @@
-#include <xc.h>
-#include "anRead.h"
-#include "buttonInterrupts.h"
-#include "servoControl.h"
+#include "dc_motor.h"
 
-#pragma config OSC = IRCIO              // Set internal clock
+#pragma config OSC = IRCIO  // internal oscillator
 
-#ifndef _XTAL_FREQ
-#define _XTAL_FREQ 8000000              // Set 8MHz clock for delay routines
-#endif
+#define PWMcycle 1 //need to calculate this
 
-//Prototype definition
-void delay_s(char seconds);     //Function to delay in seconds
+void main(void){
 
-volatile int threshold = 512;   //Define threshold to be manipulated by button interrupt
+struct DC_motor motorL, motorR; //declare 2 motor structures
 
-void main(void){   
-    OSCCON = 0x72;
-    while(!OSCCONbits.IOFS);    // Wait for OSCON to become stable
-    
-    initADC();                  //Set up analogue to digital convertor pins
-    initButtonHigh();           //Set up interrupt flags and configure them to listen on button
-    initPWM();                  //setup PWM registers
-    
-    while(1){
-        //If darker than threshold we are in night
-        if(readADC() > threshold) {
-            angle2PWM(-90);     //So move window to -90: 'closed'
-        }else { 
-            angle2PWM(90);      //Else it must be day so move to 90: 'open'
-        }
-    }
-}
+OSCCON = 0x72; //8MHz clock
+while(!OSCCONbits.IOFS); //wait until stable
 
-//delay_s abstracts repeated calls to _delay_ms to surpass limit of 87ms
-void delay_s(char seconds) {
-    for (int i = 0; i < (seconds*1000)/50; i++) {
-        __delay_ms(50);
-    }
-}
+initPWM(PWMcycle);  //setup PWM registers
 
-//InterruptHandlerHigh() enables handling of high priority interrupt signals)
-void interrupt InterruptHandlerHigh() {
-    if(INTCONbits.INT0IF) {
-        threshold = readADC();
-        INTCONbits.INT0F = 0;
-    }    
+//some code to set inital values of each structure
+
+   while(1){
+	//call your control functions, i.e. fullSpeedAhead(&motorL,&motorR);
+   }
+
 }
