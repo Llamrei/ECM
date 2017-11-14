581bab0ea1dc614c47563b4058f6dc9e6eef9f84
al3615 <al3615@mecws771-003.ic.ac.uk>
2017-11-14 13:00:43 +0000

    Lab 56 - Ex1a&b - Completed using polling

---
+++ eusart.c
@@ -0,0 +1,28 @@
+#include <xc.h>
+#include "eusart.h"
+
+void initEUSART() {
+    //set data direction registers
+    TRISCbits.RC6 = 1;
+    TRISCbits.RC7 = 1;
+       
+    // Calculation of baud rate from dependent on set values
+    SPBRG=101; //set baud rate to 9600
+    SPBRGH=0;
+    
+    // Basic config
+    BAUDCONbits.BRG16=1; //set baud rate scaling to 16 bit mode
+    TXSTAbits.BRGH=1; //high baud rate select bit
+    TXSTAbits.SYNC = 0; //Set to asynchronous
+    
+    //Finish configuration and enable
+    RCSTAbits.SPEN=1; //enable serial port, other settings default
+//    PIE1bits.RCIE =1; // enable recieve interrupts
+    TXSTAbits.TXEN=1; //enable transmitter, other settings default
+    RCSTAbits.CREN=1; //continuous receive mode
+}
+
+char getCharSerial() {
+    while(!PIR1bits.RCIF);
+    return RCREG;
+}
